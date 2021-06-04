e21f3725368fedd985cb73a2f52a2f3221a0cc4d
Isaac Moreno <ipdxcreative@gmail.com>
2021-06-04 16:30:21 -0700

    :card_file_box: apply migration for new virtual user column in model

--- BakeryTreat/Migrations/20210604232623_ApplicationUserColumnToDatabase.Designer.cs
+++ BakeryTreat/Migrations/20210604232623_ApplicationUserColumnToDatabase.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using BakeryTreat.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BakeryTreat.Migrations
 {
     [DbContext(typeof(BakeryTreatContext))]
-    partial class BakeryTreatContextModelSnapshot : ModelSnapshot
+    [Migration("20210604232623_ApplicationUserColumnToDatabase")]
+    partial class ApplicationUserColumnToDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BakeryTreat/Migrations/20210604232623_ApplicationUserColumnToDatabase.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BakeryTreat.Migrations
+{
+    public partial class ApplicationUserColumnToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TreatName",
+                table: "Treats",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FlavorName",
+                table: "Flavors",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Flavors",
+                type: "varchar(255) CHARACTER SET utf8mb4",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flavors_UserId",
+                table: "Flavors",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Flavors_AspNetUsers_UserId",
+                table: "Flavors",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Flavors_AspNetUsers_UserId",
+                table: "Flavors");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Flavors_UserId",
+                table: "Flavors");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Flavors");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TreatName",
+                table: "Treats",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FlavorName",
+                table: "Flavors",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4");
+        }
+    }
+}
