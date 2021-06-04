5602d05a134f5fa0aed823e88b174c4cff9c8855
Isaac Moreno <ipdxcreative@gmail.com>
2021-06-04 15:12:31 -0700

    :card_file_box: test migration to see if I was missing anything

---
+++ BakeryTreat/Migrations/20210604215722_CheckingForAnyChanges.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BakeryTreat.Migrations
+{
+    public partial class CheckingForAnyChanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
